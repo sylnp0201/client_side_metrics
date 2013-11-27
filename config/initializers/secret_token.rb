6705a52512d8de929406c1ae6e0aab0b54563a8f
Nianpeng Li <linianpeng@gmail.com>
2013-11-27 12:07:34 -0500

    first commit: rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ClientSideMetrics::Application.config.secret_key_base = 'e65d04d7d566017b94f193fde95e2e6a50978f9a135410bdcd093f51af00b7358136080f5388d91904e2347fbfa22784722e5af661ed8cbbf07defbe26212356'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ClientSideMetrics::Application.config.session_store :cookie_store, key: '_client_side_metrics_session'
