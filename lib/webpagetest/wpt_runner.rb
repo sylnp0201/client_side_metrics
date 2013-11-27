deca8bcec70c6672e34301fa2cf853b42689aaf9
Nianpeng Li <linianpeng@gmail.com>
2013-11-27 18:36:21 -0500

    get xml results from the tests

--- lib/webpagetest/wpt_runner.rb
+++ lib/webpagetest/wpt_runner.rb
@@ -1,5 +1,6 @@
 class Webpagetest::WptRunner
   attr_reader :options
+  attr_reader :response_records
 
   DEFAULT_LOCATIONS = ['Dulles_IE7', 'Dulles_IE8', 'Dulles_IE9', 'Dulles:Chrome']
   DEFAULT_URLS = ['http://www.bloomberg.com', 'http://www.bloomberg.com/news/2013-11-27/kamala-harris-bruises-banks-burnishes-image-with-mortgage-deals.html']
@@ -10,8 +11,10 @@ class Webpagetest::WptRunner
   end
 
   def run
-    response_records = submit_tests
-    confirm_test_submission response_records
+    submit_tests
+    confirm_submission
+    wait_and_gather_results
+    check_results
   end
 
   def submit_tests
@@ -28,10 +31,10 @@ class Webpagetest::WptRunner
         end
       end
     end
-    response_records
+    @response_records = response_records
   end
 
-  def confirm_test_submission(response_records)
+  def confirm_submission
     resp_urls = response_records.values.map{ |r| r[:url] }
     resp_locations = response_records.values.map{ |r| r[:location] }
     options[:urls].each do |url|
@@ -41,6 +44,77 @@ class Webpagetest::WptRunner
         end
       end
     end
+  end
+
+  def wait_and_gather_results
+    results = {}
+    pending_test_ids = response_records.keys
+    timeout = Time.now + time_to_wait
+    while true
+      if pending_test_ids.blank?
+        Rails.logger.info('The test is complete.')
+        break
+      end
+      if Time.now >= timeout
+        Rails.logger.error('Timeout: unable to finish the test.')
+        break
+      end
+
+      id_status = check_batch_status(pending_test_ids)
+      completed_test_ids = []
+      id_status.each_pair do |id, status_code|
+        # 1XX: Test in progress
+        # 200: Test complete
+        # 4XX: Test request not found
+        status = status_code.to_i
+        if status >= 200
+          pending_test_ids.delete(id)
+          if status == 200
+            completed_test_ids.push(id)
+          else
+            Rails.logger.error("Tests failed with status #{test_status}: #{test_id}")
+          end
+        end
+      end
+
+      test_results = gather_results(completed_test_ids)
+      results.merge!(test_results)
+      completed_test_ids.each do |id|
+        if !test_results.keys.include?(id)
+          Rails.logger.error("The XML failed to retrieve: #{id}")
+        end
+      end
+      sleep(30.seconds) if pending_test_ids.present?
+    end
+    results
+  end
+
+  def check_results
+    # this needs real implementation
+    p 'checking results'
+  end
+
+  def check_batch_status(ids)
+    status = {}
+    ids.each do |id|
+      req = "http://www.webpagetest.org/testStatus.php?f=xml&test=#{id}"
+      resp = Utils::Url.request(req, options[:proxy])
+      if resp.code == '200'
+        parser = Utils::XmlParser.new(resp.body)
+        status[id] = parser.status_code
+      end
+    end
+    status
+  end
+
+  def gather_results(ids)
+    id_dom = {}
+    ids.each do |id|
+      request = "http://www.webpagetest.org/xmlResult/#{id}/"
+      response = Utils::Url.request(request, options[:proxy])
+      id_dom[id] = response.body if response.code == '200'
+    end
+    id_dom
   end
 
   private
@@ -71,4 +145,10 @@ class Webpagetest::WptRunner
       'k'       => options[:key]
     }
   end
+
+  def time_to_wait
+    # wait for 5 mins per run per location per url
+    options[:runs] * options[:locations].size * options[:urls].size * 5 * 60
+  end
+
 end
--- spec/lib/webpagetest/wpt_runner_spec.rb
+++ spec/lib/webpagetest/wpt_runner_spec.rb
@@ -31,14 +31,14 @@ describe Webpagetest::WptRunner do
 
   describe '#submit_tests' do
     before do
-      fake_resp_body = %Q|<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<response>\n<statusCode>200</statusCode>\n<statusText>Ok</statusText>\n<data>\n<testId>131127_XB_e4ef197a13b3ebeea58b1452fbbbb3be</testId>\n<ownerKey>4acd944ed93f456537c341ed6173656213f312cb</ownerKey>\n<xmlUrl>http://www.webpagetest.org/xmlResult/131127_XB_e4ef197a13b3ebeea58b1452fbbbb3be/</xmlUrl>\n<userUrl>http://www.webpagetest.org/result/131127_XB_e4ef197a13b3ebeea58b1452fbbbb3be/</userUrl>\n<summaryCSV>http://www.webpagetest.org/result/131127_XB_e4ef197a13b3ebeea58b1452fbbbb3be/page_data.csv</summaryCSV>\n<detailCSV>http://www.webpagetest.org/result/131127_XB_e4ef197a13b3ebeea58b1452fbbbb3be/requests.csv</detailCSV>\n<jsonUrl>http://www.webpagetest.org/jsonResult.php?test=131127_XB_e4ef197a13b3ebeea58b1452fbbbb3be/</jsonUrl>\n</data>\n</response>\n|
+      fake_resp_body = Utils::FakeResponse.fake_submit
       Utils::Url.stub(:request).and_return(double(body: fake_resp_body, code: '200'))
       Webpagetest::RequestFactory.stub(:build_batch).and_return([{url: 'test_url', location: 'test_location'}])
     end
 
     it 'gets test id for submitted tests' do
       resp_recs = runner.submit_tests
-      hash = resp_recs['131127_XB_e4ef197a13b3ebeea58b1452fbbbb3be']
+      hash = resp_recs['131128_RQ_1KV']
       hash.should == {
         'location' => 'test_location',
         'url' => 'test_url' }
@@ -54,9 +54,9 @@ describe Webpagetest::WptRunner do
     end
 
     it 'logs submission failures' do
-      resp_recs = {'test_id1' => { location: 'loc1', url: 'url1'}}
+      runner.instance_variable_set(:@response_records, {'test_id1' => { location: 'loc1', url: 'url1'}})
       Rails.logger.should_receive(:error).once
-      runner.confirm_test_submission(resp_recs)
+      runner.confirm_submission
     end
 
     it 'does nothing if all submission succeed' do
@@ -64,8 +64,54 @@ describe Webpagetest::WptRunner do
         'test_id1' => { location: 'loc1', url: 'url1'},
         'test_id2' => { location: 'loc2', url: 'url1'}
       }
+      runner.instance_variable_set(:@response_records, resp_recs)
       Rails.logger.should_receive(:error).never
-      runner.confirm_test_submission(resp_recs)
+      runner.confirm_submission
+    end
+  end
+
+  describe '#check_batch_status' do
+    let (:resp1) { double(code: '200', body: Utils::FakeResponse.cnn_status)}
+    let (:resp2) { double(code: '200', body: Utils::FakeResponse.cnn_status)}
+    it 'returns status codes of the tests' do
+      Utils::Url.stub(:request).with('http://www.webpagetest.org/testStatus.php?f=xml&test=id1', nil).and_return(resp1)
+      Utils::Url.stub(:request).with('http://www.webpagetest.org/testStatus.php?f=xml&test=id2', nil).and_return(resp2)
+      runner.check_batch_status(['id1', 'id2']).should == { 'id1' => '200', 'id2' => '200' }
+    end
+  end
+
+  describe '#gather_results' do
+    it 'gets test results xml' do
+      ids = ['131128_RQ_1KV']
+      id_dom = runner.gather_results(ids)
+      id_dom[ids.first].present?.should be_true
+    end
+  end
+
+  describe '#wait_and_gather_results' do
+    let(:id1) { '131128_RQ_1KV' }
+    let(:id2) { '131128_6J_1ZF' }
+    before do
+      status1 = status2 = double(code: '200',
+                                 body: Utils::FakeResponse.cnn_status)
+      xml1 = xml2 = double(code: '200',
+                           body: Utils::FakeResponse.cnn_xml)
+      Utils::Url.stub(:request).with("http://www.webpagetest.org/testStatus.php?f=xml&test=#{id1}", nil).and_return(status1)
+      Utils::Url.stub(:request).with("http://www.webpagetest.org/testStatus.php?f=xml&test=#{id2}", nil).and_return(status2)
+      Utils::Url.stub(:request).with("http://www.webpagetest.org/xmlResult/#{id1}/", nil).and_return(xml1)
+      Utils::Url.stub(:request).with("http://www.webpagetest.org/xmlResult/#{id2}/", nil).and_return(xml2)
+    end
+    it 'does what its name says' do
+
+      resp_recs = {
+        id1 => { location: 'loc1', url: 'url1'},
+        id2 => { location: 'loc2', url: 'url1'}
+      }
+      runner.instance_variable_set(:@response_records, resp_recs)
+      results = runner.wait_and_gather_results
+      results.size.should == 2
+      results[id1].present?.should be_true
+      results[id2].present?.should be_true
     end
   end
 
